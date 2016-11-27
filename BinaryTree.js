a5e2d56c1a97060ea2db12e0b8f1591a45889574
weng <1906338209@qq.com>
2016-11-28 00:00:40 +0800

    day5

--- BinaryTree.js
+++ BinaryTree.js
@@ -38,7 +38,7 @@ function BinarySearchTree() {
     //order
     
     this.inorder = function (print) {
-        console.log("前序遍历");
+        console.log("中序遍历");
         inorderT(root,print);
     };
     var inorderT=function (node,print){
@@ -77,6 +77,97 @@ function BinarySearchTree() {
 
 
 
+    this.min = function() {
+        return minNode(root);
+    };
+
+    var minNode = function (node) {
+        if (node){
+            while (node && node.left !== null) {
+                node = node.left;
+            }
+                return node.key;
+            }
+            return null;
+        };
+
+    this.max = function() {
+        return maxNode(root);
+    };
+    var maxNode = function (node) {
+        if (node){
+            while (node && node.right !== null) {
+                node = node.right;
+            }
+            return node.key;
+        }
+        return null;
+    };
+
+
+    this.search = function(key){
+        return searchNode(root, key);
+    };
+    var searchNode = function(node, key){
+        if (node === null){
+            return false;
+        }
+        if (key < node.key){
+            return searchNode(node.left, key);
+        } else if (key > node.key){
+            return searchNode(node.right, key);
+        } else {
+            return true;
+        }
+
+    };
+
+
+
+    this.remove = function(key){
+        root = removeNode(root, key);
+    };
+
+    var removeNode = function(node, key){
+        if (node === null){
+            return null;
+        }
+        if (key < node.key){
+            node.left = removeNode(node.left, key);
+            return node;
+        } else if (key > node.key){
+            node.right = removeNode(node.right, key);
+            return node;
+        } else {
+            if (node.left === null && node.right === null){
+                node = null;
+                return node;
+            }
+            if (node.left === null){
+                node = node.right;
+                return node;
+            } else if (node.right === null){
+                node = node.left;
+                return node;
+            }
+            var mid = findminNode(node.right);
+            node.key = mid.key;
+            node.right = removeNode(node.right, mid.key);
+            return node;
+        }
+
+    };
+
+    var findminNode =function(node){
+
+            if (node){
+                while (node && node.left !== null) {
+                    node = node.left;
+                }
+                return node;
+            }
+            return null;
+    };
 
 
 
@@ -89,6 +180,15 @@ tree.insert(7);
 tree.insert(15);
 tree.insert(5);
 tree.insert(3);
+tree.insert(9);
+tree.insert(8);
+tree.insert(10);
+tree.insert(13);
+tree.insert(12);
+tree.insert(14);
+tree.insert(20);
+tree.insert(18);
+tree.insert(25);
 
 
 function print(value){
@@ -99,4 +199,14 @@ tree.inorder(print);//这里的回调函数，只调用不运行，所以不带�
 tree.preorder(print);
 tree.postorder(print);
 
+console.log("max"+tree.max());
+console.log("min"+tree.min());
 
+
+
+console.log(tree.search(1) ? 'Key 1 found.' : 'Key 1 not found.');
+console.log(tree.search(8) ? 'Key 8 found.' : 'Key 8 not found.');
+
+console.log("移除15");
+tree.remove(15);
+tree.preorder(print);
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ day3用js写了数据结构的二叉搜索树，看了一点ajax
 
 day4用hexo在github上创建了个人博客https://wengqt.github.io/
     ，并且换了个很好看的主题
+
+day5js实现二叉搜索树的搜索和删除节点，边看文档边写所以写的比较慢
